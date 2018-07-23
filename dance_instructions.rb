12c410f8dd598923e41a595aa4e52943092dfe40
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-23 21:36:37 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,7 +1,8 @@
-def How_to_take_a_photo
+def how_to_take_a_photo
   puts"get a camera"
   puts"turn on your camera"
   puts"focus your camera"
   puts "take the photo"
 end 
-How_to_take_a_photo
+
+how_to_take_a_photo
